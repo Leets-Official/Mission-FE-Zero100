d32693439b0aca20028dcadf37cd2354aa2ef53e
Saelyeam <eileen4505@gachon.ac.kr>
2025-04-01 03:16:36 +0900

    박세렴/Input.jsx 수정

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,12 +1,21 @@
+import styled from 'styled-components';
+
+const StyledInput = styled.input`
+  padding: 1rem;
+  font-size: 1rem;
+  border: 2px solid #333;
+  border-radius: 4px;
+`;
+
 function Input({ value, onChange, placeholder }) {
-    return (<input 
-              type="text" 
-              value={value} 
-              onChange={onChange} 
-              placeholder={placeholder}
-              />
-            );
-  }
-  
-  export default Input;
-  
+  return (
+    <StyledInput
+      type="text"
+      value={value}
+      onChange={onChange}
+      placeholder={placeholder}
+    />
+  );
+}
+
+export default Input;
