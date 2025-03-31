fbf845f14ad4c49e7d87a730eb953476971b0185
Saelyeam <eileen4505@gachon.ac.kr>
2025-04-01 03:16:59 +0900

    박세렴/Text.jsx 수정

--- src/components/Text.jsx
+++ src/components/Text.jsx
@@ -1,6 +1,12 @@
+import styled from 'styled-components';
+
+const StyledText = styled.span`
+  font-size: 1.1rem;
+  color: #333;
+`;
+
 function Text({ children }) {
-    return <span>{children}</span>;
-  }
-  
-  export default Text;
-  
+  return <StyledText>{children}</StyledText>;
+}
+
+export default Text;
