e4119660a4b963bf974ceb8a1d7b5d7d15c444f4
thisminn <noblesse0308@naver.com>
2025-04-01 20:35:24 +0900

    Feat: Category 버튼 스타일링 추가

---
+++ src/component/Common/Button.jsx
@@ -0,0 +1,28 @@
+import styled from 'styled-components'
+import PropTypes from 'prop-types'
+
+const Button = styled.button`
+  padding: 5px 3px;
+  margin: 5px 3px;
+  width: 80%;
+  height: 35px;
+  background-color: white;
+  color: black;
+  border: 1px solid rgba(0, 0, 0, 0.2);
+  transition:
+    background-color 0.3s ease,
+    color 0.3s ease;
+
+  &:hover {
+    background-color: #dcdcdc;
+    border: 1px solid black;
+    text-decoration: underline;
+  }
+`
+
+Button.prototype = {
+  onClick: PropTypes.func.isRequired,
+  children: PropTypes.node.isRequired,
+}
+
+export default Button
