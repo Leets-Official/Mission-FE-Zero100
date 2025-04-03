ea08d4f732da841df3e1a30e72ca882d61943912
Saelyeam <eileen4505@gachon.ac.kr>
2025-04-04 01:22:52 +0900

    feat:Checkbox 스타일 적용

--- src/components/Checkbox.jsx
+++ src/components/Checkbox.jsx
@@ -1,15 +1,34 @@
 import styled from 'styled-components';
 
-const StyledCheckbox = styled.input`
+const StyledCheckbox = styled.input.attrs({ type: 'checkbox' })`
+  appearance: none;
   width: 24px;
   height: 24px;
-  border-radius: 2px;
+  border: 2px solid #333;
+  border-radius: 4px;
+  display: grid;
+  place-content: center;
+  cursor: pointer;
+
+  &:checked {
+    background-color: black;
+  }
+
+  &:checked::before {
+    content: '✔';
+    color: white;
+    font-size: 16px;
+  }
+
+  @media (max-width: 480px) {
+    width: 20px;
+    height: 20px;
+    font-size: 14px;
+  }
 `;
 
 function Checkbox({ checked, onChange }) {
-  return (
-    <StyledCheckbox type="checkbox" checked={checked} onChange={onChange} />
-  );
+  return <StyledCheckbox checked={checked} onChange={onChange} />;
 }
 
 export default Checkbox;
