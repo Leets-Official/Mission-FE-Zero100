971dd0f20698b3311649eed7fb411a796b54f0ed
thisminn <noblesse0308@naver.com>
2025-04-06 21:08:00 +0900

    Feat: 카테고리 별 선택시 확인 가능하게 스타일링 추가

--- src/component/Category.jsx
+++ src/component/Category.jsx
@@ -1,5 +1,6 @@
-import Button from './Common/Button'
+import { useState } from 'react'
 import styled from 'styled-components'
+import Button from './Common/Button'
 
 const ButtonGroup = styled.div`
   display: flex;
@@ -9,11 +10,33 @@ const ButtonGroup = styled.div`
 `
 
 export default function Category({ onClick }) {
+  const [selected, setSelected] = useState('') // 현재 선택된 버튼 상태
+
+  const handleClick = (category) => {
+    setSelected(category) // 상태 업데이트
+    onClick(category) // 부모에게 전달
+  }
+
   return (
     <ButtonGroup>
-      <Button onClick={() => onClick('all')}>All</Button>
-      <Button onClick={() => onClick('active')}>Active</Button>
-      <Button onClick={() => onClick('completed')}>Completed</Button>
+      <Button
+        onClick={() => handleClick('all')}
+        isSelected={selected === 'all'}
+      >
+        All
+      </Button>
+      <Button
+        onClick={() => handleClick('active')}
+        isSelected={selected === 'active'}
+      >
+        Active
+      </Button>
+      <Button
+        onClick={() => handleClick('completed')}
+        isSelected={selected === 'completed'}
+      >
+        Completed
+      </Button>
     </ButtonGroup>
   )
 }
