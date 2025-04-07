fd4134b7ce7b9dec64da76df2e74d83b319e49ba
eun-seoo <kimeunseo040@gamil.com>
2025-04-07 17:01:49 +0900

    refactor: 스타일 코드 정돈 및 위치 조정

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,49 +1,47 @@
+import { useState, useContext } from 'react';
 import styled from 'styled-components';
 import Checkbox from './common/Checkbox';
 import Button from './common/Button';
-import { useContext } from 'react';
 import { TodoContext } from '../context/TodoContext';
 
 const TodoWrapper = styled.div`
   display: flex;
   flex-direction: column;
   padding: 1rem;
-  margin: 1rem 0;
   border-radius: 6px;
 `;
 
 const TopRow = styled.div`
   display: flex;
   align-items: center;
-  margin-bottom: 0.5rem;
+  gap: 0.5rem;
 `;
 
 const BottomRow = styled.div`
   display: flex;
   gap: 0.5rem;
-`;
-
-const TodoText = styled.span`
-  flex: 1;
-  margin-left: 0.5rem;
-  text-decoration: none; 
+  width: 100%; // 버튼이 반반씩 차지하도록 기준 잡기
 `;
 
 const EditInput = styled.input`
   flex: 1;
   margin-left: 0.5rem;
   padding: 4px;
+
+  /* 읽기 전용일 때 input처럼 안 보이게 */
+  border: ${({ readOnly }) => (readOnly ? 'none' : '1px solid #ccc')};
+  background-color: ${({ readOnly }) => (readOnly ? 'transparent' : 'white')};
+  font-size: 1rem;
 `;
 
 const HalfButton = styled(Button)`
-  flex: 1;
+  width: 50%; // 버튼을 정확히 반씩
 `;
 
 const Todo = ({ todo }) => {
   const { onToggle, onDelete, onEdit, onSave } = useContext(TodoContext);
   const { id, text, completed, isEditing } = todo;
 
-   // 입력값을 내부 상태로 관리
   const [editText, setEditText] = useState(text);
 
   const handleSave = () => {
@@ -51,12 +49,9 @@ const Todo = ({ todo }) => {
   };
 
   return (
-    /* TodoWarapper 한 덩어리는 할 일(Todo) 하나를 화면에 보여주는 UI임. 
-      사용자가 할 일을 완료/수정/삭제할 수 있게 해줌 */
     <TodoWrapper>
       <TopRow>
         <Checkbox checked={completed} onChange={() => onToggle(id)} />
-        {/* 컴포넌트를 하나로 유지하고, readonly 속성만 바꿔주기*/}
         <EditInput
           value={editText}
           readOnly={!isEditing}
@@ -65,15 +60,14 @@ const Todo = ({ todo }) => {
           autoFocus={isEditing}
         />
       </TopRow>
-            
+
       <BottomRow>
         <HalfButton
           variant="category"
-          onClick={() => (isEditing ? onSave(id, text) : onEdit(id))}
+          onClick={() => (isEditing ? handleSave() : onEdit(id))}
         >
           {isEditing ? 'Save' : 'Edit'}
         </HalfButton>
-
         <HalfButton variant="danger" onClick={() => onDelete(id)}>
           Delete
         </HalfButton>
