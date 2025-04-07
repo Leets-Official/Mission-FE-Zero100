22a5573e243e49240afcb780b82a5e6e0cdc2876
eun-seoo <kimeunseo040@gamil.com>
2025-04-07 16:59:04 +0900

    refactor: 왼쪽 정렬 맞추기

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -4,11 +4,14 @@ import styled from 'styled-components';
 
 const ListWrapper = styled.div`
   margin-top: 2rem;
+  padding: 0 1rem; /* 좌우 패딩 추가 */
 `;
 
 const Remaining = styled.h3`
   font-weight: bold;
+  margin-left: 1.1rem; /* 혹은 동일한 padding 사용 */
 `;
+
 
 const TodoList = ({ todos }) => {
   const remaining = todos.length;
