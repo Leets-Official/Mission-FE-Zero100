22c8eb217cec05d3559a4daa42033b66e186b90c
thisminn <noblesse0308@naver.com>
2025-04-27 14:34:41 +0900

    Feat: 컴포넌트 모듈화 파일 생성

---
+++ src/component/index.jsx
@@ -0,0 +1,21 @@
+import Text from './Common/Text.jsx'
+import Button from './Common/Button.jsx'
+import Checkbox from './Common/Checkbox.jsx'
+import Input from './Common/Input.jsx'
+import Title from './Layout/Title.jsx'
+import Category from './Category.jsx'
+import Todo from './Todo/Todo.jsx'
+import AddTodo from './Todo/AddTodo.jsx'
+import TodoList from './Todo/TodoList.jsx'
+
+export {
+  Text,
+  Button,
+  Checkbox,
+  Input,
+  Title,
+  Category,
+  Todo,
+  AddTodo,
+  TodoList,
+}
