9522fc6f270404680c9f3c95a8edff88bf385e2e
Saelyeam <eileen4505@gachon.ac.kr>
2025-05-05 12:47:34 +0900

    feat:홈 화면 구성

---
+++ src/pages/Home.jsx
@@ -0,0 +1,46 @@
+// src/pages/Home.jsx
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column; 
+  justify-content: center;
+  align-items: center;
+`;
+
+const Title = styled.h1`
+  margin-bottom: 3rem;
+`;
+
+const Button = styled(Link)`
+  margin: 0.5rem;
+  padding: 0.5rem 2rem;
+  background:rgb(211, 211, 211);
+  color: black;
+  border: none;
+  border-radius: 999px;
+  text-decoration: none;
+  font-size: 1rem;
+  font-weight: bold;
+  cursor: pointer;
+  width: 200px;  
+  text-align: center;
+
+  &:hover {
+    background:rgb(236, 236, 236);
+  }
+`;
+
+function Home() {
+  return (
+    <Wrapper>
+      <Title>OO’s TODO</Title>
+      <Button to="/login">로그인</Button>
+      <Button to="/signup">회원가입</Button>
+    </Wrapper>
+  );
+}
+
+export default Home;
