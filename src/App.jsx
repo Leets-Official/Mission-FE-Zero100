44ffc0332114307cb88f1cf07e036c0c43cd5aa4
eun-seoo <kimeunseo040@gamil.com>
2025-05-06 21:15:12 +0900

    App.jsx 리팩토링: 페이지 라우팅 기능 추가 및 TodoContext 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,10 @@
 import React, { useState, useEffect } from 'react'
-import styled from 'styled-components'
-import AddTodo from './components/AddTodo.jsx'
-import TodoList from './components/TodoList.jsx'
-import Category from './components/Category.jsx'
-import Header from './components/Header.jsx'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { TodoContext } from './context/TodoContext'
-import { v4 as uuidv4 } from 'uuid'
-
-// What needs to be done?
-const SubTitle = styled.p`
-  text-align: center;
-  font-size: 1.5rem;
-  margin: 1px 10px;
-  flex: none;
-`
-
-//전체 Todo 앱을 감싸는 큰 박스
-const AppWrapper = styled.div`
-  max-width: 600px;
-  width: 100%;
-  height: 100vh;
-  margin: auto;
-  padding: 1rem; // ✅ 기존 0 2rem → 전체 1rem로 압축
-  background-color: white;
-  //border: 1px solid #ccc;
-  box-sizing: border-box;
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-start; // ✅ 공간 넓히지 않게 변경
-  gap: 0.5rem;
-  text-align: left; // ✅ 컴포넌트 간격 압축
-  box-sizing: border-box;
-`
+import TodoPage from './pages/TodoPage'
+import MainPage from './pages/Main'
+import LoginPage from './pages/Login'
+import SignupPage from './pages/Signup'
 
 const App = () => {
   const [todos, setTodos] = useState(() => {
@@ -92,24 +65,30 @@ const App = () => {
     )
   }
 
+  const contextValue = {
+    onToggle: handleToggle,
+    onDelete: handleDelete,
+    onEdit: handleEdit,
+    onSave: handleSave,
+    onCancel: handleCancel,
+  }
+
   return (
-    <AppWrapper>
-      <TodoContext.Provider
-        value={{
-          onToggle: handleToggle,
-          onDelete: handleDelete,
-          onEdit: handleEdit,
-          onSave: handleSave,
-          onCancel: handleCancel,
-        }}
-      >
-        <Header />
-        <SubTitle>What needs to be done?</SubTitle>
-        <AddTodo setTodos={setTodos} />
-        <Category filter={filter} setFilter={setFilter} />
-        <TodoList todos={filteredTodos} />
-      </TodoContext.Provider>
-    </AppWrapper>
+    <TodoContext.Provider value={contextValue}>
+      <Router>
+        <Routes>
+          <Route path='/' element={<MainPage />} />
+          <Route path='/login' element={<LoginPage />} />
+          <Route path='/signup' element={<SignupPage />} />
+          <Route
+            path='/todo'
+            element={
+              <TodoPage todos={todos} setTodos={setTodos} filter={filter} setFilter={setFilter} />
+            }
+          />
+        </Routes>
+      </Router>
+    </TodoContext.Provider>
   )
 }
 
