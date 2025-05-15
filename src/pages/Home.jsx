376523ff6e7e44208f0791d9b79ca01243a5efaf
thisminn <noblesse0308@naver.com>
2025-05-15 18:29:41 +0900

    Feat: navigate 기능 설정

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,60 +1,67 @@
 import styled from 'styled-components'
-import { Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
+import Button from '../component/Common/Button'
+
+const Title = styled.div`
+  display: flex;
+  font-size: 30px;
+  align-items: center;
+  justify-content: center;
+  font-family: 'Lato', 'sans-serif';
+  margin-bottom: 30px;
+`
+const NewButton = styled(Button)`
+  background-color: rgba(175, 168, 168, 0.55);
+  border: 1px solid rgba(175, 168, 168, 0.55);
+  border-radius: 30px;
+  width: 250px;
+  height: 40px;
+  cursor: pointer;
+  font-weight: 600;
+  margin: 0;
+`
+
+const ButtonWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 30px;
+  font-weight: 600;
+  margin: 0;
+`
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`
 
 function Home() {
-  const Title = styled.div`
-    display: flex;
-    font-size: 30px;
-    align-items: center;
-    justify-content: center;
-    font-family: 'Lato', 'sans-serif';
-    margin-bottom: 30px;
-  `
-  const Button = styled.button`
-    color: black;
-    background-color: rgba(175, 168, 168, 0.55);
-    border: 1px solid rgba(175, 168, 168, 0.55);
-    border-radius: 30px;
-    padding: 5px 70px;
-    width: 250px;
-    height: 40px;
-  `
+  const navigate = useNavigate()
 
-  const ButtonWrapper = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    gap: 30px;
-  `
+  const goToLogin = () => {
+    navigate('/login')
+  }
 
-  const Text = styled.p`
-    font-weight: 600;
-    margin: 0;
-  `
+  const goToSignup = () => {
+    navigate('/signup')
+  }
 
-  const Wrapper = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    height: 100vh;
-  `
   return (
     <Wrapper>
       <Title>
-        <Text>민성's Todo</Text>
+        <text>민성&apos;s Todo</text>
       </Title>
       <ButtonWrapper>
-        <Link to="/login">
-          <Button>
-            <Text>로그인</Text>
-          </Button>
-        </Link>
-        <Link to="/signup">
-          <Button>
-            <Text>회원가입</Text>
-          </Button>
-        </Link>
+        <NewButton onClick={goToLogin}>
+          <text>로그인</text>
+        </NewButton>
+
+        <NewButton onClick={goToSignup}>
+          <text>회원가입</text>
+        </NewButton>
       </ButtonWrapper>
     </Wrapper>
   )
