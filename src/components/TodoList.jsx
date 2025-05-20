f3a7c5a94582f7a1a308a73c70c120339614d00a
eun-seoo <kimeunseo040@gamil.com>
2025-05-20 18:01:02 +0900

    fix: resolve merge conflict and keep Login, Main, Signup pages

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,25 +1,23 @@
-import React from 'react';
-import Todo from './Todo';
-import styled from 'styled-components';
+import React from 'react'
+import Todo from './Todo'
+import styled from 'styled-components'
 
 //<Todo /> 목록 전체를 감싸는 박스
 const ListWrapper = styled.div`
-  margin-left: 2.6rem;      // ✅ All 버튼의 왼쪽 위치와 맞춤 (조정 가능)
+  margin-left: 2.6rem; // ✅ All 버튼의 왼쪽 위치와 맞춤 (조정 가능)
   text-align: left;
-  margin-bottom: 1rem;
-`;
+`
 
 // n tasks remaining
 const Remaining = styled.h3`
   font-size: 2rem;
-  margin: 0.1rem 0.7rem 0; 
-  text-align: left;  
-  color: #444;    
-`;
-
+  margin: 0.1rem 0.7rem 0;
+  text-align: left;
+  color: #444;
+`
 
 const TodoList = ({ todos }) => {
-  const remaining = todos.length;
+  const remaining = todos.length
 
   return (
     <ListWrapper>
@@ -28,8 +26,7 @@ const TodoList = ({ todos }) => {
         <Todo key={todo.id} todo={todo} />
       ))}
     </ListWrapper>
-  );
-};
+  )
+}
 
-
-export default TodoList;
+export default TodoList
