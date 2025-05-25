c32a7658067e7528d894e0c08efd3d8b397a6a3a
thisminn <noblesse0308@naver.com>
2025-05-25 14:46:14 +0900

    Feat:배포 기능 설정

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,9 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Mission-FE-Zero100/',
   plugins: [react()],
+
   server: {
     port: 3000,
   },
